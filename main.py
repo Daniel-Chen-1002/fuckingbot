92baf94e94528cd1e8490e91595d279a16b33ad7
daniel <58624122+Daniel-Chen-903@users.noreply.github.com>
2023-09-21 20:26:32 +0800

    Update main.py

--- main.py
+++ main.py
@@ -194,16 +194,20 @@ def handle_message(event):
                     x.append(event.message.text.split(" ")[-1])
                     print(event.message.text.split(" "))
                     x.append(0)
+                    x.append(0)
+                    x.append(0)
                     team[i.user_id] = x
                     if x[0] not in totalTeams:
                         totalTeams.append(x[0])
-                season[event.message.text.split(" ")[-1]] = [1000, 0]
-        if event.message.text == "!stat" or event.message.text == "!stats" or event.message.text == "!Stat" or event.message.text == "!Stats":
+                season[event.message.text.split(" ")[-1]] = [100000, 0, 1]
+                fb.put(url, data=season, name="season")
+        if event.message.text == "stat" or event.message.text == "stats" or event.message.text == "Stat" or event.message.text == "Stats":
             line_bot_api.reply_message(event.reply_token, TextSendMessage(text=str(season)))
         if event.message.text == "!start season":
             if user == "U4e5ae01224117b28f662c288775be0a7":
                 reply = []
                 seasontime=time.time()
+                fb.put(url, data=seasontime, name="seasontime")
                 reply.append(TextSendMessage(text="Teams are devide as below:"))
                 key = list(team.keys())
                 for i in range(len(totalTeams)):
@@ -222,34 +226,68 @@ def handle_message(event):
                 if time.time()-team[user][1]>1800:
                     try:
                         reply = []
-                        if random.random()<math.log(100-season[team[user][0]][1]):
+                        if random.random()<math.log(100-season[team[user][0]][1], 100):
                             season[team[user][0]][1] = season[team[user][0]][1]+1
                             reply.append(TextSendMessage(text="Congrats, "+name+", you successfully hired a bitch for your team."))
                         else:
                             reply.append(TextSendMessage(text="Hahaha "+name+" the bitch doesn't like you!"))
                         line_bot_api.reply_message(event.reply_token, reply)
                     except KeyError:
-                        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="you are not in this season, please sign up for the next season to join."))
+                        if user not in totalTeams:
+                            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="you are not in this season, please sign up for the next season to join."))
+                        else:
+                            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Stop right there. Is your brain dead or hand broken? TYPE THE CORRECT TEAM!"))
                     team[user][1] == time.time()
                 else:
                     line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Fuck you son of a bitch, why are you in such a hurry to throw your mother to the other team? You need to wait for "+str(int((1800-(time.time()-team[user][1]))//60))+"M "+str(int((1800-(time.time()-team[user][1]))%60//1))+"S to do so."))
-            if event.message.text.split(" ")[0] == "fuck" or event.message.text.split(" ")[0] == "Fuck":
+            if event.message.text == "viagra" or event.message.text == "Viagra":
+                if time.time()-team[user][1]>1800:
+                    try:
+                        reply = []
+                        reply.append(TextSendMessage(text=name+" swallowed a pill of viagra......"))
+                        if random.randint():
+                            season[team[user][0]][2] = season[team[user][0]][2]+0.02
+                            reply.append(TextSendMessage(text="Got a small boost on his dick."))
+                        else:
+                            reply.append(TextSendMessage(text="Seems like "+name+" is very bad at chemistry, instead of C22H30N6O4S, he used C22H30N4O2S2. Nothing happened"))
+                        line_bot_api.reply_message(event.reply_token, reply)
+                    except KeyError:
+                        if user not in totalTeams:
+                            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="you are not in this season, please sign up for the next season to join."))
+                        else:
+                            line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Stop right there. Is your brain dead or hand broken? TYPE THE CORRECT TEAM!"))
+                    team[user][1] == time.time()
+                else:
+                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Fuck you son of a bitch, why are you in such a hurry to throw your mother to the other team? You need to wait for "+str(int((1800-(time.time()-team[user][1]))//60))+"M "+str(int((1800-(time.time()-team[user][1]))%60//1))+"S to do so."))
+            if time.time()-team[user][2]<=10:
+                if event.message.text == "fuck" or event.message.text == "Fuck":
+                    team[user][3]+=5
+            elif time.time()-team[user][2]>10 and team[user][3]!=0:
+                season[team[user][0]][0]+=team[user][3]
+                line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Time's up!, you got "+str(team[user][3])+"points for your team."))
+                team[user][3]==0
+            elif event.message.text.split(" ")[0] == "fuck" or event.message.text.split(" ")[0] == "Fuck":
                 try:
                     reply = []
+                    special = 0
                     if time.time()-team[user][1]>1800:
                         try:
                             if random.randint(1, 100)>season[event.message.text.split(" ")[1]][1]:
+                                if random.randint(1, 100)==50:
+                                    special = 1
                                 if random.randint(1, 20) == 1:
-                                    stole = random.randint(0, 400, 2)
+                                    stole = random.randint(0, 200)*200*season[team[user][0]][2]
                                     season[team[user][0]][0]+=stole
                                     season[event.message.text.split(" ")[1]][0] -= stole
                                     reply.append(TextSendMessage(text="💥BOOM!!!💥 YOU FUCKED THE HELL OUTTA "+event.message.text.split(" ")[1]+"'s TEAM!!!"))
                                     reply.append(TextSendMessage(text="Took extra points of "+str(stole)))
+                                    fb.put(url, data=season, name="season")
                                 else:
-                                    stole = random.randint(0, 200)
+                                    stole = random.randint(0, 200)*100*season[team[user][0]][2]
                                     season[team[user][0]][0]+=stole
                                     season[event.message.text.split(" ")[1]][0]-=stole
                                     reply.append(TextSendMessage(text="success! you fucked "+event.message.text.split(" ")[1]+"'s team and get "+str(stole)+" points."))
+                                    fb.put(url, data=season, name="season")
                             else:
                                 if random.randint(1, 5) == 1:
                                     stole = random.randint(0, 200)
@@ -257,18 +295,24 @@ def handle_message(event):
                                     season[event.message.text.split(" ")[1]][0] += stole
                                     reply.append(TextSendMessage(text="You fucked "+event.message.text.split(" ")[1]+"'s team. But your dick was stucked inside them, so your dick was yanked out of you by "+str(season[event.message.text.split(" ")[1]][1])+" bitches, they beat you up and imprisoned you."))
                                     reply.append(TextSendMessage(text="Your team paid "+str(stole)+" points for ransom."))
+                                    fb.put(url, data=season, name="season")
                                 else:
                                     reply.append(TextSendMessage(text="You tried to fuck "+event.message.text.split(" ")[1]+"'s team. But you've been beaten up and hurled away by "+str(season[event.message.text.split(" ")[1]][1])+" bitches."))
                             team[user][1]=time.time()
                         except AttributeError:
                             reply.append("Fuck you stupid idiot! Decide a team to fuck! Format:fuck theTeamToFuck")
-                        
-                        
                     else:
                         reply.append(TextSendMessage(text="Fuck you son of a bitch, you need to wait "+str(int((1800-(time.time()-team[user][1]))//60))+"M "+str(int((1800-(time.time()-team[user][1]))%60//1))+"S to fuck again."))
+                    if special == 1:
+                        reply.append(TextSendMessage(text="⚠️SPECIAL EVENT⚠️ Type as much 'fuck' as you can."))
                     line_bot_api.reply_message(event.reply_token, reply)
+                    team[user][2]==time.time()
                 except KeyError:
-                    line_bot_api.reply_message(event.reply_token, TextSendMessage(text="you are not in this season, please sign up for the next season to join."))
+                    if user not in totalTeams:
+                        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="you are not in this season, please sign up for the next season to join."))
+                    else:
+                        line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Stop right there. Is your brain dead or hand broken? TYPE THE CORRECT TEAM!"))
+                
         else:
             if seasontime!=0:
                 line_bot_api.reply_message(event.reply_token, TextSendMessage(text="Season has ended, please contact the administrator to announce the result or use !stat command to see it yourself."))
